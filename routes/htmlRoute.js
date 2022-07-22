234a10e636bec56dc03c627017e223339a54038d
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-22 01:07:49 -0700

    C:/Program Files/Git/notes path error fix

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -8,12 +8,12 @@ const router = express.Router();
 
 // Route user to notes.html (located one folder up and inside public folder) when requested by path /notes
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // Route user to index.html (located one folder up and inside public folder) when requested by path /
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // export module as router
